2437ae7a085d01c6b97e7202c641d6f9d79c5329
GKarthick121 <58224677+GKarthick121@users.noreply.github.com>
2020-01-01 22:24:23 +0000

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { slideInOutAnimation } from '../route-animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations:[slideInOutAnimation],
+  host:{'[@slideInOutAnimation]': '' }
 })
 export class HomeComponent  {
 
