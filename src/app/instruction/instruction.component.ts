360aee7008f7f01f9779afb911b812b9853731fd
GKarthick121 <58224677+GKarthick121@users.noreply.github.com>
2020-01-01 22:25:41 +0000

    Update instruction.component.ts

--- src/app/instruction/instruction.component.ts
+++ src/app/instruction/instruction.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { slideInOutAnimation } from '../route-animations';
 
 @Component({
   selector: 'app-instruction',
   templateUrl: './instruction.component.html',
-  styleUrls: ['./instruction.component.scss']
+  styleUrls: ['./instruction.component.scss'],
+  animations:[slideInOutAnimation],
+  host:{'[@slideInOutAnimation]': '' }
 })
 export class InstructionComponent  {
 
